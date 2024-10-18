7f624779f063bff382c9cb1d50fb6e5ed9cdecb8
fabio <fabioi@stanford.edu>
2024-10-18 09:27:50 -0700

    feat: release assignment 3

---
+++ assign3/main.cpp
@@ -0,0 +1,17 @@
+/*
+ * CS106L Assignment 3: Make a Class
+ * Created by Fabio Ibanez with modifications by Jacob Roberts-Baca.
+ */
+
+/* #### Please don't change this line! #### */
+int run_autograder();
+
+int main() {
+  // STUDENT TODO: Construct an instance of your class!
+
+  /* #### Please don't change this line! #### */
+  return run_autograder();
+}
+
+/* #### Please don't change this line! #### */
+#include "utils.hpp"
