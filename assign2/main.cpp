68b3d199cf4816b60ffae213b6f4f57b55c4b6e6
Nicoledon <3121506708@qq.com>
2025-01-29 14:02:48 +0800

    acomplish assign 2

--- assign2/main.cpp
+++ assign2/main.cpp
@@ -16,7 +16,7 @@
 
 #include "utils.h"
 
-std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
+std::string kYourName = "sleepy joe"; // Don't forget to change this!
 
 /**
  * Takes in a file name and returns a set containing all of the applicant names as a set.
@@ -30,7 +30,14 @@ std::string kYourName = "STUDENT TODO"; // Don't forget to change this!
  * to also change the corresponding functions in `utils.h`.
  */
 std::set<std::string> get_applicants(std::string filename) {
-  // STUDENT TODO: Implement this function.
+  // STUDENT TODO: Implement this function. 
+ /**/ auto set = std::set<std::string>();
+  std::ifstream file(filename);
+  std::string name;
+  while(std::getline(file, name)) {
+       set.insert(name);
+  }
+  return set;
 }
 
 /**
@@ -41,8 +48,20 @@ std::set<std::string> get_applicants(std::string filename) {
  * @param students  The set of student names.
  * @return          A queue containing pointers to each matching name.
  */
+bool ShareInitials(std::string s ,std::string r){
+   return s[0] == r[0];
+}
 std::queue<const std::string*> find_matches(std::string name, std::set<std::string>& students) {
   // STUDENT TODO: Implement this function.
+  auto queue = std::queue<const std::string*>();
+  for( auto item : students){
+      if(ShareInitials(name,item)){
+            std::string *ptr = new std::string{item};
+            std::string elem = *ptr;
+            queue.push(ptr);
+      }
+  }
+  return queue;
 }
 
 /**
@@ -57,7 +76,22 @@ std::queue<const std::string*> find_matches(std::string name, std::set<std::stri
  */
 std::string get_match(std::queue<const std::string*>& matches) {
   // STUDENT TODO: Implement this function.
+  if(!matches.empty())
+  {
+      const std::string *item = matches.front();
+      std::string elem = *item;
+      return elem;
+  }
+  std::string str = "NO MATCHES FOUND.";
+   return str;
 }
 
 /* #### Please don't modify this call to the autograder! #### */
-int main() { return run_autograder(); }
+int main() {
+   // std::set<std::string>sets = get_applicants("/home/jeff/cs106l-assignments/assign2/students.txt");
+    //std::queue<const std::string*>m = find_matches("A",sets);
+  //  get_match(m);
+   return run_autograder();
+  // return 0;
+}
+
