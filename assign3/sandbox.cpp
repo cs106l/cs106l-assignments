ff7df0990fa0209655884353c46552b2d0c8f935
JTRB <jacobrobertsbaca@gmail.com>
2025-01-31 13:52:56 -0800

    feat: assignment 3 wip

---
+++ assign3/sandbox.cpp
@@ -0,0 +1,8 @@
+/*
+ * CS106L Assignment 3: Make a Class
+ * Created by Fabio Ibanez with modifications by Jacob Roberts-Baca.
+ */
+
+void sandbox() {
+  // STUDENT TODO: Construct an instance of your class!
+}
